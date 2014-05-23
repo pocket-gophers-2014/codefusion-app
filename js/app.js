c5d4d7237c6709bbe6b660cc74606e34beaf79d3
Aaron Wertman <aaron.wertman@gmail.com>
2014-05-23 00:02:54 -0700

    Added model refreshing based on firebase response   * classrooms route will reload with new info TODO:   * individual classroom route (e.g: classroom/2) will not update with updated FB details. requires refiltering process

--- js/app.js
+++ js/app.js
@@ -7,35 +7,52 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    // return ['red', 'yellow', 'blue'];
   }
 });
 
 App.ClassroomsRoute = Ember.Route.extend({
   model: function() {
-    return data;
+    return App.ClassroomHolder;
   }
 });
 
 App.ClassroomRoute = Ember.Route.extend({
   model: function(params) {
-    // var shirt = data.findBy( 'classroom_id', params.classroom_id );
-
-    var things = fb.on("value", function(data) {
-      name = data.val() ? data.val().name : "";
-      console.log("Name is: " + name)
-      return name
-    })
-    console.log(things)
-    return { things: things }
+    return App.ClassroomHolder.findBy('classroom_id', params.classroom_id)
   }
 });
 
-// var data = [{classroom_id: "1", name: "fun"}, {classroom_id: "2", name: "less fun"}]
-var fb = new Firebase("https://radiant-fire-3325.firebaseio.com/");
-// fb.set({ classroom_id: "2", name: "pants" })
 
-// App.ApplicationAdapter = DS.FirebaseAdapter.extend({
-//   firebase: new Firebase("https://radiant-fire-3325.firebaseio.com/")
-// });
-// App.ApplicationSerializer = DS.FirebaseSerializer.extend();
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+App.ClassroomHolder = Ember.ArrayController.create({
+  content: []
+})
+
+App.ClassRooms = Ember.Object.extend({
+  // classroomObserver: function() {
+    // console.log("observer notified")
+  // }.observes('name').on('init')
+})
+
+
+var roomOne = App.ClassRooms.create({
+  classroom_id: "1",
+  name: "less fun"
+})
+App.ClassroomHolder.pushObject(roomOne)
+
+var fb = new Firebase("https://radiant-fire-3325.firebaseio.com/");
+fb.on("value", function(data) {
+  formattedData = data.val() ? data.val().name : "";
+  newRoom = App.ClassRooms.create({classroom_id: data.val().classroom_id, name: data.val().name })
+  console.log(newRoom)
+  App.ClassroomHolder.pushObject(newRoom)
+  // console.log(App.ClassroomHolder.content)
+})
+
+
+
