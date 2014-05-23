650f0d4a33370b01ad91487a22a1e1d20e01c4ca
Aaron Wertman <aaron.wertman@gmail.com>
2014-05-23 11:08:02 -0700

    added method for testing setting new classrooms

--- js/app.js
+++ js/app.js
@@ -19,40 +19,37 @@ App.ClassroomsRoute = Ember.Route.extend({
 
 App.ClassroomRoute = Ember.Route.extend({
   model: function(params) {
-    return App.ClassroomHolder.findBy('classroom_id', params.classroom_id)
+    return App.ClassroomHolder//.findBy('classroom_id', params.classroom_id)
   }
 });
-
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 App.ClassroomHolder = Ember.ArrayController.create({
-  content: []
+  // content: []
 })
 
-App.ClassRooms = Ember.Object.extend({
+App.ClassRoom = Ember.Object.extend({
   // classroomObserver: function() {
     // console.log("observer notified")
   // }.observes('name').on('init')
 })
 
-
-var roomOne = App.ClassRooms.create({
-  classroom_id: "1",
-  name: "less fun"
-})
-App.ClassroomHolder.pushObject(roomOne)
-
 var fb = new Firebase("https://radiant-fire-3325.firebaseio.com/");
 fb.on("value", function(data) {
   formattedData = data.val() ? data.val().name : "";
-  newRoom = App.ClassRooms.create({classroom_id: data.val().classroom_id, name: data.val().name })
+  newRoom = App.ClassRoom.create({classroom_id: data.val().classroom_id, name: data.val().name })
   console.log(newRoom)
   App.ClassroomHolder.pushObject(newRoom)
   // console.log(App.ClassroomHolder.content)
 })
 
+// fb.set({ classroom_id: "2", name: "pants" })
 
-
+var roomOne = App.ClassRoom.create({
+  classroom_id: "1",
+  name: "less fun"
+})
+App.ClassroomHolder.pushObject(roomOne)
