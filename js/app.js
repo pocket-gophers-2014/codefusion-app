9bfb6e5ef66f2b8589a9a21df0c3990c53078611
James Hulley <jameshulley@JAH-MBP1.local>
2014-05-22 20:26:06 -0700

    Test firebase stuff

--- js/app.js
+++ js/app.js
@@ -19,8 +19,23 @@ App.ClassroomsRoute = Ember.Route.extend({
 
 App.ClassroomRoute = Ember.Route.extend({
   model: function(params) {
-    return data.findBy( 'classroom_id', params.classroom_id );
+    // var shirt = data.findBy( 'classroom_id', params.classroom_id );
+
+    var things = fb.on("value", function(data) {
+      name = data.val() ? data.val().name : "";
+      console.log("Name is: " + name)
+      return name
+    })
+    console.log(things)
+    return { things: things }
   }
 });
 
-var data = [{classroom_id: "1", name: "fun"}, {classroom_id: "2", name: "less fun"}]
+// var data = [{classroom_id: "1", name: "fun"}, {classroom_id: "2", name: "less fun"}]
+var fb = new Firebase("https://radiant-fire-3325.firebaseio.com/");
+// fb.set({ classroom_id: "2", name: "pants" })
+
+// App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+//   firebase: new Firebase("https://radiant-fire-3325.firebaseio.com/")
+// });
+// App.ApplicationSerializer = DS.FirebaseSerializer.extend();
