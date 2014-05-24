85d3cfca700d2e351dea07fbef205d16fea3d18f
James Hulley <jameshulley@JAH-MBP1.local>
2014-05-23 23:20:31 -0700

    Testing heroku without server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var application = express();
+// var express = require('express');
+// var application = express();
 
-application.configure(function() {
-  application.use(express.static(__dirname));
-  application.use(express.logger('dev'));
-  application.use(express.bodyParser());
-});
+// application.configure(function() {
+//   application.use(express.static(__dirname));
+//   application.use(express.logger('dev'));
+//   application.use(express.bodyParser());
+// });
 
-// application.get('*', function() {
-//   res.sendfile('index.html')
-// })
+// // application.get('*', function() {
+// //   res.sendfile('index.html')
+// // })
 
-application.listen(8080)
-console.log("---------- App listening on port 8080 ----------")
+// application.listen(8080)
+// console.log("---------- App listening on port 8080 ----------")
