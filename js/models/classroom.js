423dfff3fc2d08ec0e557914bea4f684b4ac3135
Aaron Wertman <aaron.wertman@gmail.com>
2014-05-24 19:00:39 -0700

    added methods for self updating for models

--- js/models/classroom.js
+++ js/models/classroom.js
@@ -1,6 +1,12 @@
 App.CurrentClassroom = Ember.Object.extend({
   classroom_code: "",
-  file_content: ""
+  file_content: "",
+  file_name: "",
+  updateAttributes: function(params) {
+    this.set("classroom_code", (params.classroom_code || ""))
+    this.set("file_content", (params.file_content || ""))
+    this.set("file_name", (params.file_name || ""))
+  }
 }).create()
 
 
