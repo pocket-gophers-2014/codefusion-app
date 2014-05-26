4c81affd361b0b21bebaccc0f4ed05d8840133f1
Aaron Wertman <aaron.wertman@gmail.com>
2014-05-25 21:07:53 -0700

    minor line spacing refactor

--- js/controllers/classroomcontroller.js
+++ js/controllers/classroomcontroller.js
@@ -1,12 +1,13 @@
 // trying to figure out how to get rid of one of these..
 App.ClassroomsRoute = Ember.Route.extend({
   model: function() {
+    App.MasterViewController.initializeNoteListener()
     return App.CurrentClassroom;
   }
 });
 
 App.ClassroomRoute = Ember.Route.extend({
   model: function(params) {
-    App.FireBaseController.singleRequest(params["classroom_id"]) // this is firebase data updates
+    App.FireBaseController.singleRequest(params["classroom_id"])
   }
 });
