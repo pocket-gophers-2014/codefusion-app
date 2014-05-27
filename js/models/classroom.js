80da55c7155d4be635e113c6ebde1a65cc119fe2
Aaron Wertman <aaron.wertman@gmail.com>
2014-05-27 02:17:23 -0700

    minor line reorganization

--- js/models/classroom.js
+++ js/models/classroom.js
@@ -1,8 +1,8 @@
 App.CurrentClassroom = Ember.Object.extend({
   classroom_code: "",
   content: "",
-  currentCode: "",
-  currentNote: "",
+  currentCode: "", // option
+  currentNote: "", // option
   updateAttributes: function(params) {
     this.set('classroom_code', params.classroom_code)
     this.set('content', params.content)
