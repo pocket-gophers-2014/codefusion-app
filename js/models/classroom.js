7183abf7bcf3aa08bd1c7a2ddbf67bf97c2ef26d
David Stavis <davidstavis@gmail.com>
2014-05-28 02:01:56 -0700

    Successfully saves a recursively generated zip file   *The zip file repeatedly nests the root directy. Why?   *All layers of folders are nested within one another. How do we get breadth as well as depth?

--- js/models/classroom.js
+++ js/models/classroom.js
@@ -1,4 +1,36 @@
 App.Classroom = Ember.Object.extend({
   content: null,
-  classroomCode: null
+  classroomCode: null,
+  asZipDirectory: function(){
+    var files = this.content.files
+    var folders = this.content.folders
+    var folderName = this.content.folder_name
+
+    var zip = new JSZip();
+    zip = zip.folder(folderName)
+    zip = this.buildFiles(zip, files)
+    zip = this.buildFolders(zip, folders)
+    return zip
+  },
+  buildFolders: function(zip, folders){
+    if(folders){
+      folders.forEach(function(subFolder){
+        zip = zip.folder(subFolder.folder_name)
+        this.buildFiles(zip, subFolder.files)
+        if(subFolder.folders !== undefined){ //if it has subfolders, build them
+          this.buildFolders(zip, subFolder.folders)
+        }
+      }, this)
+    }
+    return zip
+  },
+
+  buildFiles: function(zip, files){
+    if(files){
+      files.forEach(function(file){
+        zip = zip.file(file.file_name, file.file_content)
+      }, this)
+    }
+    return zip
+  }
 }).create();
