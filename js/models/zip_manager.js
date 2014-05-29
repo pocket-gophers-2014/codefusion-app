7b54546e425c2e6f47e78ec4639bb8a95df9256e
David Stavis <davidstavis@gmail.com>
2014-05-28 21:36:40 -0700

    RED WIP: Zip download refactor

---
+++ js/models/zip_manager.js
@@ -0,0 +1,54 @@
+//Replace filesaver.js and jszip with minified versions or with CDN references
+App.ZipManager = Ember.Object.extend({
+  getZip: function(rootDirectory){
+    return this.makeZip(rootDirectory)
+  },
+  makeZip: function(rootDirectory){
+    var downloadable = buildFileStructure(rootDirectory).generate({type:'blob'}, this.nameTheZipFileName(rootDirectory))
+    return downloadable
+  },
+  buildFileStructure: function(rootDirectory){ 
+    var zip = new JSZip().folder(rootDirectory.folder_name)
+    this.addCumulativeNotesToZip(zip)
+    this.addFilesToZip(zip, rootDirectory.files)
+    this.addFoldersToZip(zip, rootDirectory.folders)
+    return zip
+  },
+  addFoldersToZip: function(zip, folders){
+    if(folders !== undefined){
+      folders.forEach(function(subFolder){
+        var zipSubFolder = this.makeZipSubFolder(zip, subFolder)
+        this.addFilesToZip(zipSubFolder, subFolder.files)
+
+        if(subFolder.folders !== undefined){ 
+          this.addFoldersToZip(zipSubFolder, subFolder.folders)
+        }
+      }, this)
+    }
+    return zip
+  },
+  addFilesToZip: function(zip, files){
+    if(files){
+      files.forEach(function(file){
+        zip = zip.file(this.truncatePathName(file.file_name), file.file_content)
+      }, this)
+    }
+    return zip
+  },
+  makeZipSubFolder: function(zip){
+    return zip.folder(this.truncatePathName(subFolder.folder_name))
+  },
+  addCumulativeNotesToZip: function(zip){
+    zip.file(this.nameTheNotesFile(), App.NoteHolder.generateCumulativeNotes())
+    return zip
+  },
+  truncatePathName: function(pathName){
+    return pathName.slice(pathName.lastIndexOf('/') + 1, 500)
+  },
+  nameTheZipFile: function(rootDirectory){
+    return (App.Classroom.classroomCode + "_" + rootDirectory.folder_name + ".zip")
+  },
+  nameTheNotesFile: function(){
+    App.Classroom.classroomCode + "_notes.txt"
+  }
+}).create()
